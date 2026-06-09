a5a07519a6048ea4fe52849884aa36417918242c
mohammadmahdiziyafat <mohammadmahdiziafat@gmail.com>
2026-06-10 01:28:06 +0330

    install reactrouter for contacts project

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,10 +1,17 @@
-import spinnerGif from '../assets/Spinner.gif';
+import spinnerGif from "../assets/Spinner.gif";
 const Spinner = () => {
-    return (
-        // use reactfragment
-        <>
-            <img src={spinnerGif} alt='spinner' className='d-block m-auto' style={{ width: '200px' }}></img>
-        </>
-    )
-}
-export default Spinner
+  return (
+    // use reactfragment
+    <>
+      <img
+        src={spinnerGif}
+        alt="spinner"
+        className="d-block m-auto"
+        style={{
+          width: "300px",
+        }}
+      ></img>
+    </>
+  );
+};
+export default Spinner;
