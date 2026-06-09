b069e9e970fb6350832dc1efbb525540b854a7a0
mohammadmahdiziyafat <mohammadmahdiziafat@gmail.com>
2026-06-10 01:39:25 +0330

    creat Spiner

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,9 +2,9 @@ import spinnerGif from '../assets/Spinner.gif';
 const Spinner = () => {
     return (
         // use reactfragment
-        <>
+        <React.Fragment>
             <img src={spinnerGif} alt='spinner' className='d-block m-auto' style={{ width: '200px' }}></img>
-        </>
+        </React.Fragment>
     )
 }
 export default Spinner
